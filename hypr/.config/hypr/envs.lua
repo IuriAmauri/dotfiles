84d8c7b9cb5761c0fc2bdb6ed2aaabe6ac246fa5
Iuri Amauri Girolometo <iuri_amauri@hotmail.com>
2026-09-02 13:08:34 -0400

    Configure to use rootless docker

---
+++ hypr/.config/hypr/envs.lua
@@ -0,0 +1,2 @@
+hl.env("LIBVA_DRIVER_NAME", "iHD")
+hl.env("OMARCHY_DOCKER_SOCKET", "/run/user/1000/docker.sock")
--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -1,14 +1,12 @@
--- Omarchy's bootstrap keeps path setup out of this user config.
 dofile((os.getenv("OMARCHY_PATH") or "/usr/share/omarchy") .. "/default/hypr/bootstrap.lua")
 
--- Load Omarchy defaults.
 require("default.hypr.omarchy")
 
+require("hypr.envs")
 require("hypr.monitors")
 require("hypr.input")
 require("hypr.bindings")
 require("hypr.looknfeel")
 require("hypr.autostart")
 
--- Toggle config flags dynamically.
 require("default.hypr.toggles")
